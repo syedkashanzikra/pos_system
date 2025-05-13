ef16d55b9510ef437bf66b2153c4f8605944eb53
SyedKashanAbbasNaqvi <abbaskashan234@gmail.com>
2025-05-13 21:02:27 +0500

    export working done ✔️

---
+++ app/Exports/ProductLedgerExport.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Exports;
+
+use App\Models\ProductLedger;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+
+class ProductLedgerExport implements FromCollection, WithHeadings
+{
+    protected $productId;
+
+    public function __construct($productId)
+    {
+        $this->productId = $productId;
+    }
+
+    public function collection()
+    {
+        return ProductLedger::where('product_id', $this->productId)
+            ->select('date', 'type', 'reference', 'quantity_in', 'quantity_out', 'balance')
+            ->orderBy('date')
+            ->get();
+    }
+
+    public function headings(): array
+    {
+        return ['Date', 'Type', 'Reference', 'In', 'Out', 'Balance'];
+    }
+}
